48488b428f4d27c9ac8a693ea82d604413ad1b8e
Gabriel_Vieira <nox030705@gmail.com>
2024-08-29 10:51:14 -0400

    feat(js): added filter of users by followers count

---
+++ src/js/lib/filterUsers.js
@@ -0,0 +1,28 @@
+import renderUsers from "./renderUsers.js";
+
+/**
+ * @typedef {Object} User
+ * @property {string} username - User name
+ * @property {string} img - User image url
+ * @property {int} followers_count - Followers count
+ * @property {int} repos_count - Repositories count
+ * */
+/**
+ * Filter users by followers
+ * @param {User[]} users - Users array
+ * @param {HTMLElement} $container - Users element container
+ * */
+export default function filterUsers(users, $container) {
+    const $followers = document.querySelector("main > aside > form > select");
+
+    $followers.addEventListener("change", () => {
+        const value = $followers.value;
+        const usersSorted = users.sort((userA, userB) => {
+            return value == "greater"
+                ? userB.followers_count - userA.followers_count
+                : userA.followers_count - userB.followers_count;
+        });
+        $container.innerHTML = ""
+        renderUsers(usersSorted, $container)
+    });
+}
--- src/js/search.js
+++ src/js/search.js
@@ -3,6 +3,7 @@ import userImage from "./lib/userImage.js";
 import fetchServer from "./lib/fetch.js";
 import renderUsers from "./lib/renderUsers.js";
 import showErrorModal from "./lib/errorModal.js";
+import filterUsers from "./lib/filterUsers.js";
 
 headerSearch();
 userImage();
@@ -20,14 +21,17 @@ const res = await fetchServer(
     },
 );
 
-$section.innerHTML = ""
+$section.innerHTML = "";
 if (res.code == 200) {
     renderUsers(res.result.data, $section);
+    filterUsers(res.result.data, $section);
 } else if (res.code == 404) {
-    const $message = document.createElement("p")
-    $message.innerText = `No se encontraron usuarios con el nombre: ${urlParam.get("username")}`
-    $section.appendChild($message)
+    const $message = document.createElement("p");
+    $message.innerText = `No se encontraron usuarios con el nombre: ${urlParam.get("username")}`;
+    $section.appendChild($message);
 } else {
-    showErrorModal(res.result.message,{href:"../pages/dashboard.html",message: "Dashboard"})
+    showErrorModal(res.result.message, {
+        href: "../pages/dashboard.html",
+        message: "Dashboard",
+    });
 }
-
