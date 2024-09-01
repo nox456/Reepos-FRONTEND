63cc4ecfb6278a94fe8b0ed8c11b81159f779c36
Gabriel_Vieira <nox030705@gmail.com>
2024-09-01 13:43:06 -0400

    feat(search): search in mobile showed by a button

--- src/js/lib/headerSearch.js
+++ src/js/lib/headerSearch.js
@@ -2,24 +2,53 @@
  * Change the placeholder and href of the header search input
  * */
 export default function headerSearch() {
-    const $form = document.querySelector("header form")
+    const $form = document.querySelector("header form");
 
-    const $input = $form.querySelector("input")
+    const $input = $form.querySelector("input");
 
-    const $select = $form.querySelector("select")
+    const $select = $form.querySelector("select");
 
     $select.addEventListener("change", () => {
-        const value = $select.value
-        $input.placeholder = `Explora ${value == "repositories" ? "repositorios" : "usuarios"}...`
-    })
+        const value = $select.value;
+        $input.placeholder = `Explora ${value == "repositories" ? "repositorios" : "usuarios"}...`;
+    });
 
     $form.addEventListener("submit", async (e) => {
-        e.preventDefault()
-        const value = $select.value
+        e.preventDefault();
+        const value = $select.value;
         if (value == "repositories") {
-            location.href = `../../pages/search_${value}?repoName=${$input.value}`
+            location.href = `../../pages/search_${value}?repoName=${$input.value}`;
         } else {
-            location.href = `../../pages/search_${value}?username=${$input.value}`
+            location.href = `../../pages/search_${value}?username=${$input.value}`;
         }
-    })
+    });
+
+    const $search_button = document.querySelector("header > button");
+    const $dialog = document.createElement("dialog");
+    $dialog.id = "dialog-search-form";
+    $form.id = "search-form";
+    $dialog.appendChild($form);
+
+    const $submit_button = document.createElement("button");
+    $submit_button.setAttribute("form", "search-form");
+
+    $submit_button.classList.add("link");
+    $submit_button.innerText = "Buscar";
+
+    $dialog.appendChild($submit_button);
+
+    $search_button.addEventListener("click", () => {
+        document.body.insertAdjacentElement("afterbegin", $dialog);
+        $dialog.showModal();
+    });
+    document.body.addEventListener("click", (e) => {
+        if (
+            e.target == $dialog &&
+            e.target.closest("button") != $search_button &&
+            Array.from(document.body.children).includes($dialog)
+        ) {
+            $dialog.close();
+            $dialog.remove()
+        }
+    });
 }
