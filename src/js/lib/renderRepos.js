966e8ff52f7c9402fd7fffe41777b16326113f76
Gabriel_Vieira <nox030705@gmail.com>
2024-09-16 11:51:47 -0400

    feat(repos): basic info of repository in repo page

--- src/js/lib/renderRepos.js
+++ src/js/lib/renderRepos.js
@@ -17,16 +17,18 @@ import langColors from "./langColors.js";
 export default async function renderRepos(repos,$container) {
     return new Promise((resolve) => {
         const lang_list = new Set()
+        const urlParams = new URLSearchParams(location.href.slice(location.href.indexOf("?")))
         repos.forEach(repo => {
             const $article = document.createElement("article")
             const date = new Date(repo.created_at)
             const date_formatted = `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}, ${date.getHours()}:${date.getMinutes()}`
+            repo.username ??= urlParams.get("username")
             $article.classList.add("repository")
             $article.innerHTML = `
                 <header>
                     <svg viewBox="0 0 24 24"><path d="M19 2.01H6c-1.206 0-3 .799-3 3v14c0 2.201 1.794 3 3 3h15v-2H6.012C5.55 19.998 5 19.815 5 19.01c0-.101.009-.191.024-.273.112-.575.583-.717.987-.727H20c.018 0 .031-.009.049-.01H21V4.01c0-1.103-.897-2-2-2zm0 14H5v-11c0-.806.55-.988 1-1h7v7l2-1 2 1v-7h2v12z"></path></svg>
                     <h2>
-                        <a href="../pages/repository?repoName=${repo.name}&username=${repo.user}" >${repo.name}</a>
+                        <a href="../pages/repository?repoName=${repo.name}&username=${repo.username}" >${repo.name}</a>
                     </h2>
                     <span>${date_formatted}</span>
                 </header>
@@ -43,11 +45,11 @@ export default async function renderRepos(repos,$container) {
                     </section>
                 </footer>
             `
-            if (repo.user) {
+            if (location.href.includes("search")) {
                 const headerLink = $article.querySelector("header > h2")
                 const usernameLink = document.createElement("a")
-                usernameLink.href = `../pages/profile?username=${repo.user}`
-                usernameLink.innerText = repo.user
+                usernameLink.href = `../pages/profile?username=${repo.username}`
+                usernameLink.innerText = repo.username
                 headerLink.insertAdjacentElement("afterbegin",usernameLink)
                 usernameLink.insertAdjacentHTML("afterend","<span> / </span>")
             }
--- src/js/repository.js
+++ src/js/repository.js
@@ -1,6 +1,7 @@
 import fetchServer from "./lib/fetch.js"
 import headerSearch from "./lib/headerSearch.js"
 import userImage from "./lib/userImage.js"
+import user from "./is_authenticated.js"
 
 headerSearch()
 userImage()
@@ -18,5 +19,28 @@ const $main = document.querySelector("body > main")
 const $section1 = $main.querySelector("section:nth-child(1)")
 
 const $image = $section1.querySelector("div:nth-of-type(1) img")
+const $username = $section1.querySelector("div:nth-of-type(1) a")
+const $repoName = $section1.querySelector("div:nth-of-type(1) span")
+const $config = $section1.querySelector("div:nth-of-type(2) a")
+const $likes = $section1.querySelector("div:nth-of-type(2) button")
 
 $image.src = user_response.result.data.user_img
+$username.innerText = urlParams.get("username")
+$username.href = `../pages/profile?username=${urlParams.get("username")}`
+$repoName.innerText = urlParams.get("repoName")
+$config.href = `../pages/config?repoName=${urlParams.get("repoName")}`
+$likes.querySelector("span").innerText = repo_response.result.data.likes
+
+$likes.addEventListener("click", async () => {
+    const username = user.username
+    const userOwnerName = urlParams.get("username")
+    const res = await fetchServer("/repositories/like", {
+        method: "PUT",
+        body: {repoName: urlParams.get("repoName"),username,userOwnerName}
+    })
+    if (res.code != 200) {
+        console.log(res)
+    } else {
+        location.reload()
+    }
+})
