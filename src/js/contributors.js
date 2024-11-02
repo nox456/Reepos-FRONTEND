a109946606ff481253d8fba6e6d5fc247424c7e7
Gabriel_Vieira <nox030705@gmail.com>
2024-11-02 01:26:09 -0400

    feat(contributors): added commits created filter

--- src/js/contributors.js
+++ src/js/contributors.js
@@ -2,6 +2,8 @@ import userImage from "./lib/userImage.js";
 import headerSearch from "./lib/headerSearch.js";
 import fetchServer from "./lib/fetch.js";
 import showErrorModal from "./lib/errorModal.js";
+import renderContributors from "./lib/renderContributors.js";
+import filterContributors from "./lib/filterContributors.js";
 
 userImage();
 headerSearch();
@@ -53,25 +55,10 @@ const $section2 = document.querySelector(
     "body > main > section:nth-of-type(2)",
 );
 
+const $form = $section2.querySelector("form")
+
 const $list = document.querySelector("body > main > ul");
 
-contributors_response.result.data.forEach((contributor) => {
-    const $element = document.createElement("li");
-    const $name = document.createElement("span");
-    $name.innerHTML = `
-        <svg viewBox="0 0 24 24"><path d="M12 2a5 5 0 1 0 5 5 5 5 0 0 0-5-5zm0 8a3 3 0 1 1 3-3 3 3 0 0 1-3 3zm9 11v-1a7 7 0 0 0-7-7h-4a7 7 0 0 0-7 7v1h2v-1a5 5 0 0 1 5-5h4a5 5 0 0 1 5 5v1z"></path></svg>
-        ${contributor.name}
-    `;
-    const $last_commit = document.createElement("span");
-    $last_commit.innerText = contributor.last_commit_title;
-    const $commits = document.createElement("span");
-    $commits.innerHTML = `
-        ${contributor.commits_created}
-        <svg viewBox="0 0 24 24"><path d="M12 6c-2.967 0-5.431 2.167-5.909 5H2v2h4.092c.479 2.832 2.942 4.998 5.909 4.998s5.43-2.166 5.909-4.998H22v-2h-4.09c-.478-2.833-2.942-5-5.91-5zm0 9.998c-2.205 0-3.999-1.794-3.999-3.999S9.795 8 12 8c2.206 0 4 1.794 4 3.999s-1.794 3.999-4 3.999z"></path></svg>
-    `
+renderContributors(contributors_response.result.data,$list)
 
-    $element.appendChild($name);
-    $element.appendChild($last_commit);
-    $element.appendChild($commits);
-    $list.appendChild($element);
-});
+filterContributors(contributors_response.result.data,$list,$form)
---
+++ src/js/lib/filterContributors.js
@@ -0,0 +1,15 @@
+import renderContributors from "./renderContributors.js"
+
+export default function filterContributors(contributors,$container,$form) {
+    const $select = $form.querySelector("select")
+
+    $select.addEventListener("change",() => {
+        const value = $select.value
+
+        const contributorsOrdered = contributors.sort((a,b) => value == "more" ? b.commits_created - a.commits_created : a.commits_created - b.commits_created)
+
+        $container.innerHTML = ""
+        renderContributors(contributorsOrdered,$container)
+    })
+
+}
