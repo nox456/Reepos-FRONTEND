cb3a3aa4fa0890616641fc720fd4cb026f42cef8
Gabriel_Vieira <nox030705@gmail.com>
2024-11-02 22:14:32 -0400

    feat(file): show file info and download button

--- src/js/file.js
+++ src/js/file.js
@@ -1,6 +1,7 @@
 import fetchServer from "./lib/fetch.js";
 import headerSearch from "./lib/headerSearch.js";
 import userImage from "./lib/userImage.js";
+import timeago from "./lib/timeago.js"
 
 headerSearch();
 userImage();
@@ -22,7 +23,7 @@ const file = file_response.result.data
 
 const extension = file.name.slice(file.name.lastIndexOf(".") + 1)
 
-const $pre = document.createElement("pre")
+const $pre = document.querySelector("body > main > pre")
 $pre.classList.add("line-numbers")
 const $code = document.createElement("code")
 
@@ -35,10 +36,28 @@ if (extension == "html") {
 }
 $pre.appendChild($code)
 
-document.querySelector("body > main").insertAdjacentElement("afterbegin",$pre)
-
 document.head.insertAdjacentHTML("beforeend",`<link rel="stylesheet" href="../css/prism.css">`)
 
 await import("../js/lib/prism.js")
 
-console.log(file)
+const $name_size = document.querySelector("body > main > section > p:nth-of-type(1)")
+$name_size.insertAdjacentText("beforeend",`${file.name} - ${file.size}`)
+
+const $lines = document.querySelector("body > main > section > p:nth-of-type(2)")
+$lines.insertAdjacentText("beforeend",file.content.split("\n").length - 1)
+
+const $commit_title = document.querySelector("main > section > p:nth-of-type(3) > a")
+$commit_title.href = `../pages/commit?hash=${file.last_commit.hash}&repoName=${repoName}&username=${username}`
+$commit_title.insertAdjacentText("beforeend",file.last_commit.title)
+
+const $commit_date = document.querySelector("main > section > p:nth-of-type(3) > span")
+
+const date = new Date(file.last_commit.created_at)
+$commit_date.insertAdjacentText("beforeend",timeago({
+    year: date.getFullYear(),
+    month: date.getMonth(),
+    day: date.getDate()
+}))
+
+const $download = document.querySelector("main > section > a")
+$download.href = file.url
