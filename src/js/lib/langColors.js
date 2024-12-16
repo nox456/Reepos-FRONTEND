ded1bde1b00470e8b5310ab0a3971264af6adf8c
Gabriel Vieira <nox030705@gmail.com>
2024-12-15 23:57:59 -0400

    feat(languages): added extra languages

--- src/js/lib/langColors.js
+++ src/js/lib/langColors.js
@@ -2,6 +2,16 @@ const langColors = {
     "javascript": "#f1e05a",
     "html": "#e34c26",
     "css": "#563d7c",
-    "sql": "#e38c00"
+    "sql": "#e38c00",
+    "python": "#3572a5",
+    "c++": "#f34b7d",
+    "c#": "#178600",
+    "c": "#555555",
+    "ruby": "#701516",
+    "go": "#00add8",
+    "java": "#b07219",
+    "typescript": "#3178c6",
+    "lua": "#000080",
+    "php": "#4f5d95",
 }
 export default langColors
